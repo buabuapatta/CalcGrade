61bda550872dca70411232dbfee639117f894e91
DESKTOP-43AVUG3\BAIBUA <Patharapond@gmail.com>
2023-08-28 14:51:20 +0700

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'screen/Home.dart';
+import 'package:firebase_core/firebase_core.dart'; // Import the Firebase library
 
-void main() {
+
+void main() async{
+  
+  WidgetsFlutterBinding.ensureInitialized(); // Ensure that Flutter is initialized
+  await Firebase.initializeApp(); // Initialize Firebase
+  
   runApp(const MyApp());
 }
 
