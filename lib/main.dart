0b885ce2ebb756293e19f904b50f489846e76acc
DESKTOP-43AVUG3\BAIBUA <Patharapond@gmail.com>
2023-08-29 13:46:22 +0700

    update for web

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'screen/Home.dart';
-import 'package:firebase_core/firebase_core.dart'; // Import the Firebase library
+import 'package:firebase_core/firebase_core.dart';
 
-
-void main() async{
-  
-  WidgetsFlutterBinding.ensureInitialized(); // Ensure that Flutter is initialized
-  await Firebase.initializeApp(); // Initialize Firebase
-  
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: FirebaseOptions(
+          apiKey: "AIzaSyDcPF6cMX-UY8VGQhkxs3NMDUTqsiosRpo",
+          authDomain: "flutterproject-46003.firebaseapp.com",
+          projectId: "flutterproject-46003",
+          storageBucket: "flutterproject-46003.appspot.com",
+          messagingSenderId: "130650583417",
+          appId: "1:130650583417:web:d77e769b3359c65a32ffa8",
+          measurementId: "G-R062YFCFZ5"));
   runApp(const MyApp());
+  
 }
 
 class MyApp extends StatelessWidget {
